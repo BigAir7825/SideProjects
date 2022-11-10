86496be77d41c624b7d57fd2cd658bd34be629f5
Will Adair <88009623+BigAir7825@users.noreply.github.com>
2022-11-10 13:28:15 -0600

    FileSorterApplication - Initial commit

---
+++ FileSorterApplication/FileSorterApplication/FileWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSorterApplication
+{
+    internal class FileWork
+    {
+        public List<string> rawData;
+        public FileWork(string purpose, string filePath, string textToWrite)
+        {
+            switch (purpose)
+            {
+                case "read":
+                    ReadFromFile(filePath);
+                    break;
+                case "write":
+                    break;
+            }
+        }
+        public void ReadFromFile(string filePath)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(filePath);
+                while (!reader.EndOfStream)
+                    rawData.Add(reader.ReadLine());
+                reader.Close();
+            } catch (Exception e)
+            {
+                Console.WriteLine("File path not set up");
+                InitialSetup setupApplication = new InitialSetup("error");
+            }
+        }
+        public void WriteToFile(string filePath, string textToWrite)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.Write(textToWrite);
+            writer.Close();
+        }
+    }
+}
---
+++ FileSorterApplication/FileSorterApplication/InitialSetup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSorterApplication
+{
+    internal class InitialSetup
+    {
+        public string userName;
+        Dictionary<string, string> paths;
+        public InitialSetup(string condition)
+        {
+            switch (condition)
+            {
+                case "error":
+                    FromError();
+                    break;
+                case "normal":
+                    NormalOperations();
+                    break;
+            }
+
+        }
+        public void FromError()
+        {
+            Console.Write("Initial setup incomplete\nEnter the user name for the account (i.e. C\\Users\\{Username}): ");
+            userName = Console.ReadLine();
+        }
+        public void NormalOperations()
+        {
+            paths = new Dictionary<string, string>();
+            FileWork fileWork = new FileWork("read", "paths.txt", "");
+            foreach (string line in fileWork.rawData)
+            {
+                string[] splitLine = line.Split(" - ");
+                switch (splitLine[0])
+                {
+                    case "College":
+                        break;
+                    case "1) Freshman":
+                        break;
+                    case "2) Sophomore":
+                        break;
+                    case "3) Junior":
+                        break;
+                    case "4) Senior":
+                        break;
+                    case "1) Fall":
+                        break;
+                    case "2) Spring":
+                        break;
+                    case "3) Summer":
+                        break;
+                }
+            }
+        }
+    }
+}
