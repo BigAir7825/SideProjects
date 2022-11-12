47036fe35f6762f584fe9ed4c9e0ad1000ff90c6
Will Adair <88009623+WillHAdair@users.noreply.github.com>
2022-11-11 23:19:32 -0700

    FileSorterApplication - improved InitialSetup.cs
    Added some more code to allow the user to continue creating sub-folders until the smallest sub-folder type

--- FileSorterApplication/FileSorterApplication/InitialSetup.cs
+++ FileSorterApplication/FileSorterApplication/InitialSetup.cs
@@ -38,20 +38,46 @@ namespace FileSorterApplication
         {
             string collegePath = makeValidFilePath(originalFilePath, getInput("Enter the name of the college folder: "));
             toPrint.Add("COLLEGE", collegePath);
-            for (int collegeYear = 1; collegeYear <= Convert.ToInt32(getInput("Enter the number of years in college (reccomended 4): ")); collegeYear++)
+            int numberOfYears = Convert.ToInt32(getInput("Enter the number of years in college (reccomended 4): "));
+            for (int collegeYear = 1; collegeYear <= numberOfYears; collegeYear++)
                 getYearInfo(collegePath, collegeYear);
         }
         public void getYearInfo(string collegeFilePath, int collegeYear)
         {
             string yearPath = makeValidFilePath(collegeFilePath, getInput($"Enter the name for year {collegeYear}: "));
             toPrint.Add("YEAR", yearPath);
-            for (int yearSemester = 1; yearSemester <= Convert.ToInt32(getInput($"Enter the number of semesters for year {collegeYear}: ")); yearSemester++)
+            int numberOfSemesters = Convert.ToInt32(getInput($"Enter the number of semesters for year {collegeYear}: "));
+            for (int yearSemester = 1; yearSemester <= numberOfSemesters; yearSemester++)
                 getSemesterInfo(yearPath, yearSemester, collegeYear);
         }
         public void getSemesterInfo(string yearFilePath, int yearSemester, int collegeYear)
         {
             string semesterPath = makeValidFilePath(yearFilePath, getInput($"Enter the name for semester {yearSemester} of {collegeYear}: "));
             toPrint.Add("SEMESTER", semesterPath);
+            int numberOfFocusAreas = Convert.ToInt32(getInput($"Enter the number of focus areas for year {collegeYear}, semester {yearSemester} (i.e. classes & internship): "));
+            for (int semesterFocus = 1; semesterFocus <= numberOfFocusAreas; semesterFocus++)
+                getFocusAreaInfo(semesterPath, semesterFocus, yearSemester, collegeYear);
+        }
+        public void getFocusAreaInfo(string semesterFilePath, int semesterFocus, int yearSemester, int collegeYear)
+        {
+            string focusAreaPath = makeValidFilePath(semesterFilePath, getInput($"Enter the name for focus area {semesterFocus} of semester {yearSemester} of year {collegeYear}: "));
+            toPrint.Add("FOCUSAREA", focusAreaPath);
+            int numberOfSubjects = Convert.ToInt32(getInput($"Enter the number of subjects in focus areas for year {collegeYear}, semester {yearSemester}, focus area {semesterFocus} (i.e. number of classes): "));
+            for (int subjectFocus = 1; subjectFocus <= numberOfSubjects; subjectFocus++)
+                getSubjectInfo(focusAreaPath, subjectFocus, semesterFocus, yearSemester, collegeYear);
+        }
+        public void getSubjectInfo(string focusAreaFilePath, int subjectFocus, int semesterFocus, int yearSemester, int collegeYear)
+        {
+            string subjectPath = makeValidFilePath(focusAreaFilePath, getInput($"Enter the name for subject {subjectFocus} of focus area {semesterFocus} of semester {yearSemester} of year {collegeYear}: "));
+            toPrint.Add("SUBJECT", subjectPath);
+            int numberOfSections = Convert.ToInt32(getInput($"Enter the number of sections in subjects for year {collegeYear}, semester {yearSemester}, focus area {semesterFocus} (i.e. number of lab/lectures): "));
+            for (int sectionSubject = 1; sectionSubject <= numberOfSections; sectionSubject++)
+                getSectionInfo(subjectPath, sectionSubject, subjectFocus, semesterFocus, yearSemester, collegeYear);
+        }
+        public void getSectionInfo(string subjectFilePath, int sectionSubject, int subjectFocus, int semesterFocus, int yearSemester, int collegeYear)
+        {
+            string sectionPath = makeValidFilePath(subjectFilePath, getInput($"Enter the name for section {sectionSubject} of subject {subjectFocus} of focus area {semesterFocus} of semester {yearSemester} of year {collegeYear}: "));
+            toPrint.Add("SECTION", sectionPath);
         }
     }
 }
